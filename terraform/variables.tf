9e58b28acf450ac23b201479f0b45a52b4b20a7e
Fode Mangane <fodemangane@gmail.com>
2025-06-26 08:23:14 +0000

    ok tt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,7 +22,7 @@ variable "environment" {
 variable "aws_region" {
   description = "Région AWS"
   type        = string
-  default     = "eu-west-1"
+  default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
